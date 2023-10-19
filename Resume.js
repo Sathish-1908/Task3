907d97bd515abbcf9ffcfaa60b07b2e55eb43404
Sathish-1908 <kumarsathish.k19@gmail.com>
2023-10-19 06:08:56 +0530

    creating JSON for resume

---
+++ Resume.js
@@ -0,0 +1,39 @@
+// creating a JSON for resume:
+
+let resume = new Object();
+
+resume = {
+  personal: {
+    name: "sathish",
+    age: 23,
+    gender: "male",
+    contactNum: "1234567890",
+    mailId: "xyz@gmail.com",
+  },
+  education: {
+    10: {
+      schoolName: "Kavitha Mat Hr Sec School",
+      percentage: 89.4,
+    },
+    12: {
+      schoolName: "Kavitha Mat Hr Sec School",
+      percentage: 71.3,
+    },
+    ug: {
+      collegeName: "SRM Arts And Science College",
+      percentage: 73.76,
+    },
+  },
+  experience: {
+    companyName: "CAMS",
+    experience: "2Years",
+    designation: "Executive",
+  },
+  skill: `HTML
+          CSS
+          JAVASCRIPT
+          REACTJS
+          NODEJS  `,
+};
+
+//console.log(resume);
